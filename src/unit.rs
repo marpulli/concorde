f965d4c03b1aff95afb5a4eb0219c2cb8f8b4d54
Mark Pullin <33632490+marpulli@users.noreply.github.com>
2025-10-17 09:46:58 +0100

    Modify Unit class

--- src/unit.rs
+++ src/unit.rs
@@ -1,74 +1,126 @@
-use std::fmt;
+use ordered_float::OrderedFloat;
+use std::{collections::HashMap, fmt};
 
 pub struct Unit {
-    // TODO: Consider whether we need to support custom dimensions
-    dimension_exponents: [f64; 7],
-
-    scale_factor: f64,
-
     // TODO: consider using rational numbers for exponent and strong type for the unit
     // The string here represents a "defined unit" that is part of the registry
-    // Also consider using a map here. We expect the vectors to be short so I don't expect much
-    // speed improvement and the ordering is important, so maybe not worth it.
-    components: Vec<(String, f64)>,
+    components: std::collections::HashMap<String, f64>,
 }
 
-#[derive(Debug)]
-pub struct IncompatibleUnitError {
-    pub expected: String,
-    pub found: String,
-}
+// Dimensions are: Length, Mass, Time, ELectric Current, temperature, amount of substance, luminous intensity
 
-impl fmt::Display for IncompatibleUnitError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "Incompatible units: expected {}, found {}",
-            self.expected, self.found
-        )
-    }
+pub struct DefinedUnit {
+    name: String,
+    dimensions: [f64; 7],
+    // TODO: this implies only "linear" units are used.
+    // We probably want a more generic affine unit support, or even non-linear unit conversion scales
+    scale: f64,
+    aliases: Vec<String>,
 }
-
-impl std::error::Error for IncompatibleUnitError {}
 
 impl Unit {
-    pub fn has_same_dimensions(&self, other: &Self) -> bool {
-        return other.dimension_exponents == self.dimension_exponents;
+    pub fn new(units: HashMap<String, f64>) -> Unit {
+        if units.is_empty() {
+            return Unit {
+                components: HashMap::new(),
+            };
+        }
+
+        let mut result_components = HashMap::new();
+        for (defined_unit, exponent) in units {
+            result_components.insert(defined_unit, exponent);
+        }
+        // filter zero
+        result_components.retain(|_, &mut value| value != 0.0);
+
+        Unit {
+            components: result_components,
+        }
     }
 
     pub fn mul(&self, other: &Self) -> Self {
-        let new_dimension =
-            std::array::from_fn(|i| self.dimension_exponents[i] + other.dimension_exponents[i]);
+        // The new unit components is a sum of the units of "self" and "other"
+        // This code ensures that if both units have the same components, they are summed and
+        // any components that sum to zero are removed
+        let mut component_map = std::collections::HashMap::new();
 
-        let mut new_components = Vec::with_capacity(self.components.len() + other.components.len());
+        // Add all components from both units
         for (component, value) in &self.components {
-            let other_value = other
-                .components
-                .iter()
-                .find(|(k, _)| k == component)
-                .map(|(_, v)| v)
-                .unwrap_or(&0.0);
-            let sum = other_value + value;
-            if other_value + value != 0.0 {
-                new_components.push((component.clone(), other_value + value))
-            }
+            *component_map.entry(component.clone()).or_insert(0.0) += value;
+        }
+        for (component, value) in &other.components {
+            *component_map.entry(component.clone()).or_insert(0.0) += value;
         }
 
-        for (component, value) in &other.components {
-            if (self
-                .components
-                .iter()
-                .find(|(k, _)| k == component)
-                .is_none())
-            {
-                new_components.push((component.clone(), *value));
-            }
-        }
+        // filter out zeros
+        component_map.retain(|_, &mut v| v != 0.0);
 
         return Unit {
-            dimension_exponents: new_dimension,
-            scale_factor: self.scale_factor * other.scale_factor,
-            components: new_components,
+            components: component_map,
         };
+    }
+
+    pub fn div(&self, other: &Self) -> Self {
+        // The new unit components is a sum of the units of "self" and "other"
+        // This code ensures that if both units have the same components, they are summed and
+        // any components that sum to zero are removed
+        let mut component_map = std::collections::HashMap::new();
+
+        // Add all components from both units
+        for (component, value) in &self.components {
+            *component_map.entry(component.clone()).or_insert(0.0) += value;
+        }
+        for (component, value) in &other.components {
+            *component_map.entry(component.clone()).or_insert(0.0) -= value;
+        }
+
+        // filter out zeros
+        component_map.retain(|_, &mut v| v != 0.0);
+
+        return Unit {
+            components: component_map,
+        };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_mul_two_kg_unit() {
+        // You'd need to modify DefinedUnit to use OrderedFloat<f64> for scale
+        let kg = DefinedUnit {
+            name: "kg".to_string(),
+            dimensions: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            scale: 1.0,
+            aliases: vec![],
+        };
+
+        let unit_1 = Unit::new(HashMap::from([(kg.name.clone(), 1.0)]));
+        let unit_2 = Unit::new(HashMap::from([(kg.name.clone(), 1.0)]));
+
+        let unit_3 = unit_1.mul(&unit_2);
+        let kg_exponent = unit_3.components.get(&"kg".to_string());
+        assert!(kg_exponent == Some(&2.0))
+    }
+
+    #[test]
+    fn test_div_two_kg_unit() {
+        // You'd need to modify DefinedUnit to use OrderedFloat<f64> for scale
+        let kg = DefinedUnit {
+            name: "kg".to_string(),
+            dimensions: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+            scale: 1.0,
+            aliases: vec![],
+        };
+
+        let unit_1 = Unit::new(HashMap::from([(kg.name.clone(), 1.0)]));
+        let unit_2 = Unit::new(HashMap::from([(kg.name.clone(), 1.0)]));
+
+        let unit_3 = unit_1.div(&unit_2);
+        let kg_exponent = unit_3.components.get(&"kg".to_string());
+        assert!(kg_exponent.iter().len() == 0)
     }
 }
